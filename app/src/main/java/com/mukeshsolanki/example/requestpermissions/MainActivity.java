d2b5b905fdddcf1b3118df984cb6bfa5a62075c1
Mukesh <mukesh@keepworks.com>
2016-02-22 16:00:59 +0530

    Updated to library version 0.0.3

--- app/src/main/java/com/mukeshsolanki/example/requestpermissions/MainActivity.java
+++ app/src/main/java/com/mukeshsolanki/example/requestpermissions/MainActivity.java
@@ -10,27 +10,33 @@ import com.mukeshsolanki.requestpermissions.RuntimePermission;
 
 public class MainActivity extends AppCompatActivity {
   public static final String[] PERMISSIONS = {
-      Manifest.permission.READ_PHONE_STATE, Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS, Manifest.permission.WRITE_EXTERNAL_STORAGE
+      Manifest.permission.READ_PHONE_STATE, Manifest.permission.RECEIVE_SMS,
+      Manifest.permission.READ_SMS, Manifest.permission.WRITE_EXTERNAL_STORAGE
   };
+  private static final String TAG = MainActivity.class.getName();
   private int mRequestCode = 100;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     RuntimePermission runtimePermission = new RuntimePermission();
-    runtimePermission.requestPermissions(this, PERMISSIONS, mRequestCode);
+    if (runtimePermission.hasPermission(this, PERMISSIONS)) {
+      Log.d(TAG, "All Permissions Granted");
+    } else {
+      runtimePermission.requestPermission(this, PERMISSIONS, mRequestCode);
+    }
   }
 
-  @Override public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+  @Override public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+      @NonNull int[] grantResults) {
     super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     if (requestCode == mRequestCode) {
       for (int grantResult : grantResults) {
         if (grantResult == PackageManager.PERMISSION_DENIED) {
-          Log.d("PermissionResult=>", "Denied");
+          Log.d(TAG, "Denied");
           return;
         }
       }
-      Log.d("PermissionResult=>", "All Permissions Granted");
     }
   }
 }
